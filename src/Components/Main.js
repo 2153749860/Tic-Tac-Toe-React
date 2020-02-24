cf9515a68d7ad2d535d0c3a53e4558249b17e013
TheNumberGuy <tiagorocha1515@gmail.com>
2020-02-24 14:37:48 +0000

    Footer component added

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Game from './Game';
+import Footer from './Footer';
 
 export default function Main() {
   return (
     <div>
         <Header/>
         <Game/>
+        <Footer/>
     </div>
   );
 }
