bad2f2554e20f03667022e384b0afbc3eae09b63
TheNumberGuy <tiagorocha1515@gmail.com>
2020-02-24 14:31:47 +0000

    First Route added

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Main from './Components/Main';
 
 export default function Routes() {
   return (
     <BrowserRouter>
-        <Route path="/" exact />
+        <Route path="/" exact component={Main}/>
     </BrowserRouter>
   );
 }
