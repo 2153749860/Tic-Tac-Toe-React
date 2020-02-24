94c0c8345c1ffc7fb1ccd60324daf74e7f0acbc6
TheNumberGuy <tiagorocha1515@gmail.com>
2020-02-24 14:26:02 +0000

    Routes added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,6 +4,8 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div></div>
+    <div classname="header">
+        Galo
+    </div>
   );
 }
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+        <Route path="/" exact />
+    </BrowserRouter>
+  );
+}
