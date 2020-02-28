6dcb3764d0654b8a89d8f893f9697c142e66f9fd
RAJ66 <vitorbarbosa1998@hotmail.com>
2020-02-28 00:22:38 +0000

    header complete

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
-
+import "./Header.css";
+//https://coolors.co/ac3931-e5d352-d9e76c-537d8d-482c3d
 export default function Header() {
-  return (
-    <div className="header-container">
-        Galo
-    </div>
-  );
+  return <header className="header-container">Tic-Tac-Toe</header>;
 }
